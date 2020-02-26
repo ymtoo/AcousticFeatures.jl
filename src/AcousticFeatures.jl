3a5d73f6b612843b71bba315ab92dbc10f2c9a17
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2020-02-26 18:50:10 +0800

    Switch to Peaks.jl

--- src/AcousticFeatures.jl
+++ src/AcousticFeatures.jl
@@ -5,7 +5,7 @@ include("utils.jl")
 
 using .Subsequences, .Utils
 
-using AlphaStableDistributions, DSP, Findpeaks, StatsBase
+using AlphaStableDistributions, DSP, Peaks, StatsBase
 
 export Energy, Myriad, FrequencyContours, Score, Subsequence
 
@@ -76,11 +76,11 @@ function score(f::FrequencyContours, x::AbstractVector{T}) where T<:Real
     δf = frequency[2]-frequency[1]
     f.tnorm === nothing ? Nnorm = size(p, 2) : Nnorm = f.tnorm÷(δt) |> Int
     p    = spectrumflatten(p, Nnorm) #noise-flattened spectrogram
-    crds = findpeaks(p[:, 1], minHeight=percentile(p[:, 1], f.minhprc), minDist=trunc(Int, f.minfdist÷δf))
+    crds,_ = peakprom(p[:, 1], Maxima(), trunc(Int, f.minfdist÷δf), percentile(p[:, 1], f.minhprc))
     ctrs = [[(crd[1], 1)] for crd in crds]
     for (i, col) in enumerate(eachcol(p[:, 2:end]))
         col = collect(col)
-        crds = findpeaks(col, minHeight=percentile(col, f.minhprc), minDist=trunc(Int, f.minfdist/δf))
+        crds,_ = Peaks.peakprom(col, Maxima(), trunc(Int, f.minfdist/δf), percentile(col, f.minhprc))
         for crd in crds
             if length(ctrs) == 0
                 ctrs = [[(crd[1], 1)] for crd in crds]
