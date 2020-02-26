d2c697d99807f782b2da734ed9eae13731bd75a8
ymtoo <ymtoo86@gmail.com>
2020-02-26 23:13:36 +0800

    Add length of Subsequence

--- src/subsequences.jl
+++ src/subsequences.jl
@@ -3,24 +3,32 @@ module Subsequences
 export Subsequence
 
 struct Subsequence
-    c
+    c::AbstractVector
     winlen::Int
     noverlap::Int
-    Subsequence(c, winlen, noverlap) = (mod(winlen, 2) == 0) ? new(c, winlen+1, noverlap) : new(c, winlen, noverlap)
+    step::Int
+    padlen::Int
+end
+
+function Subsequence(c, winlen, noverlap)
+    winlen = (mod(winlen, 2) == 0) ? winlen+1 : winlen
+    winlen > length(c) && throw(ArgumentError("`winlen` has to be smaller than the signal length."))
+    step = winlen-noverlap
+    padlen = winlen÷2
+    Subsequence(c, winlen, noverlap, step, padlen)
 end
 
 function Base.iterate(itr::Subsequence, state=1)
     state > length(itr.c) && return nothing
-    itr.winlen > length(itr.c) && throw(ArgumentError("`winlen` has to be smaller than the signal length."))
-    step = itr.winlen-itr.noverlap
-    padlen = itr.winlen÷2
-    if state <= padlen
-        return vcat(zeros(eltype(itr.c), padlen-(state-1)), itr.c[1:state+padlen]), state+step
-    elseif state >= length(itr.c)-padlen
-        return vcat(itr.c[state-padlen:end], zeros(eltype(itr.c), padlen-(length(itr.c)-state))), state+step
+    if state <= itr.padlen
+        return vcat(zeros(eltype(itr.c), itr.padlen-(state-1)), itr.c[1:state+itr.padlen]), state+itr.step
+    elseif state >= length(itr.c)-itr.padlen
+        return vcat(itr.c[state-itr.padlen:end], zeros(eltype(itr.c), itr.padlen-(length(itr.c)-state))), state+itr.step
     else
-        return itr.c[state-padlen:state+padlen], state+step
+        return itr.c[state-itr.padlen:state+itr.padlen], state+itr.step
     end
 end
+
+Base.length(itr::Subsequence) = ceil(Int64, length(itr.c)/itr.step)
 
 end
