c0e4d7619f62606eb47e57aefbab2f61e8427892
ymtoo <ymtoo86@gmail.com>
2020-06-05 14:26:47 +0800

    fix(subsequence): slow indexing on PaddedView of LazyWAVFile

--- src/subsequences.jl
+++ src/subsequences.jl
@@ -1,7 +1,6 @@
-using PaddedViews
-
-struct Subsequence{VT <: AbstractVector}
+struct Subsequence{VT<:AbstractVector,T}
     c::VT
+    fillvalue::T
     winlen::Int
     noverlap::Int
     step::Int
@@ -19,25 +18,33 @@ function getpadlen(winlen)
     lpadlen, rpadlen
 end
 
-function Subsequence(c::AbstractVector{T}, winlen, noverlap; fill=zero(T)) where T
+function Subsequence(c::AbstractVector{T}, winlen, noverlap; fillvalue=zero(T)) where T
     winlen > length(c) && throw(ArgumentError("`winlen` has to be smaller than the signal length."))
     step = winlen-noverlap
     lpadlen, rpadlen = getpadlen(winlen)
-    Subsequence(PaddedView(fill, c, (1-lpadlen:length(c)+rpadlen,)), winlen, noverlap, step, lpadlen, rpadlen)
+    Subsequence(c,
+                fillvalue,
+                winlen,
+                noverlap,
+                step,
+                lpadlen,
+                rpadlen)
+    # Subsequence(PaddedView(fill, c, (1-lpadlen:length(c)+rpadlen,)), winlen, noverlap, step, lpadlen, rpadlen)
 end
 
 function Base.iterate(subseq::Subsequence, state=1)
-    state > length(subseq.c.data) && return nothing
-    return @view(subseq.c[state-subseq.lpadlen:state+subseq.rpadlen]), state+subseq.step
-    # if state <= subseq.lpadlen
-    #     return vcat(zeros(eltype(subseq.c), subseq.lpadlen-(state-1)), subseq.c[1:state+subseq.rpadlen]), state+subseq.step
-    # elseif state >= lenc-subseq.rpadlen
-    #     return vcat(subseq.c[state-subseq.lpadlen:end], zeros(eltype(subseq.c), subseq.rpadlen-(lenc-state))), state+subseq.step
-    # else
-    #     return subseq.c[state-subseq.lpadlen:state+subseq.rpadlen], state+subseq.step
-    # end
+    lenc = length(subseq.c)
+    state > lenc && return nothing
+    # return @view(subseq.c[state-subseq.lpadlen:state+subseq.rpadlen]), state+subseq.step
+    if state <= subseq.lpadlen
+        return vcat(fill(subseq.fillvalue, subseq.lpadlen-(state-1)), @view(subseq.c[1:state+subseq.rpadlen])), state+subseq.step
+    elseif state >= lenc-subseq.rpadlen
+        return vcat(@view(subseq.c[state-subseq.lpadlen:end]), fill(subseq.fillvalue, subseq.rpadlen-(lenc-state))), state+subseq.step
+    else
+        return @view(subseq.c[state-subseq.lpadlen:state+subseq.rpadlen]), state+subseq.step
+    end
 end
 
-Base.length(subseq::Subsequence) = ceil(Int64, length(subseq.c.data)/subseq.step)
+Base.length(subseq::Subsequence) = ceil(Int64, length(subseq.c)/subseq.step)
 
-Base.getindex(subseq::Subsequence, i::Number) = iterate(subseq, (1:subseq.step:length(subseq.c.data))[i])[1]
+Base.getindex(subseq::Subsequence, i::Number) = iterate(subseq, (1:subseq.step:length(subseq.c))[i])[1]
